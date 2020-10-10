53af029143e55ce37741de15073a7e31b527eaa7
shashankj99 <shashankj677@gmail.com>
2020-10-10 09:26:35 +0545

    installed material-ui/core package

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Typography} from '@material-ui/core';
+
+function App() {
+    return (
+        <Typography component='h1' variant='h5'>
+            Hello World
+        </Typography>
+    );
+}
+
+export default App;
